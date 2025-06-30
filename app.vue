1ff7019b467913eb6279688cc80145bf4d4e83f3
miha-new <miha-new@yandex.ru>
2025-06-30 15:54:41 +0300

    Checking API Auth

--- app.vue
+++ app.vue
@@ -9,68 +9,64 @@
   </div>
 </template>
 
-<script>
-export default {
-  setup() {
-    const API_KEY = '8fe8ba4797cf9dd9f53aad6b4fe11134'
-    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+<script setup>
+import {ref} from 'vue'
 
-    async function getRequestToken() {
-      const response = await fetch(
-        `https://api.themoviedb.org/3/authentication/token/new?api_key=${API_KEY}`
-      )
-      const data = await response.json();
-      console.log('request_token', data.request_token)
-      return data.request_token;
-    }
+const API_KEY = '8fe8ba4797cf9dd9f53aad6b4fe11134'
+const url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
 
-    async function handlerLogin(event, value) {
-      console.log(event, value)
-      const requestToken = await getRequestToken();
+const formLoginName = ref('')
+const formLoginPassword = ref('')
 
-      const isValid = await validateCredentials(requestToken, username, password);
-      
-      if (isValid) {
-        const sessionId = await createSession(requestToken);
-        localStorage.setItem('tmdb_session', sessionId);
-        alert('success');
-        loadUserData();
-      } else {
-        alert('error');
-      }
-    };
-
-    async function validateCredentials(token, username, password) {
-      const response = await fetch(
-        `https://api.themoviedb.org/3/authentication/token/validate_with_login?api_key=${API_KEY}`,
-        {
-          method: 'POST',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            username: username,
-            password: password,
-            request_token: token
-          })
-        }
-      );
-      
-      return response.ok;
-    }
-
-
-
-    fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-      })
-      .catch(error => {
-        console.error('Error', error)
-      })
-
-    return {
-      handlerLogin
-    }
-  }
+async function getRequestToken() {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/authentication/token/new?api_key=${API_KEY}`
+  )
+  const data = await response.json();
+  console.log('request_token', data.request_token)
+  return data.request_token;
 }
+
+async function handlerLogin() {
+  const requestToken = await getRequestToken();
+  console.log(formLoginName, formLoginPassword)
+  const isValid = await validateCredentials(requestToken, formLoginName, formLoginPassword);
+  
+  if (isValid) {
+    // const sessionId = await createSession(requestToken);
+    // localStorage.setItem('tmdb_session', sessionId);
+    alert('success');
+    // loadUserData();
+  } else {
+    alert('error');
+  }
+};
+
+async function validateCredentials(token, username, password) {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/authentication/token/validate_with_login?api_key=${API_KEY}`,
+    {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        username: username,
+        password: password,
+        request_token: token
+      })
+    }
+  );
+  
+  return response.ok;
+}
+
+
+
+fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+  })
+  .catch(error => {
+    console.error('Error', error)
+  })
 </script>
