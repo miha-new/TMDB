e81dc2d703016d1486360950635a57db24d93400
miha-new <miha-new@yandex.ru>
2025-06-30 15:30:28 +0300

    Checking API Auth

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <button @click="redirectToAuth">Login</button>
+    <form @submit.prevent="handlerLogin">
+      <input type="text" placeholder="Name" required>
+      <input type="password" placeholder="Password" required>
+      <button type="submit">Войти</button>
+    </form>
     <NuxtPage />
   </div>
 </template>
@@ -10,50 +14,49 @@ export default {
   setup() {
     const API_KEY = '8fe8ba4797cf9dd9f53aad6b4fe11134'
     const url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
-    const REDIRECT_URL = 'https://tmdb-emnuolha7-mihanews-projects.vercel.app/auth-callback';
 
     async function getRequestToken() {
       const response = await fetch(
         `https://api.themoviedb.org/3/authentication/token/new?api_key=${API_KEY}`
-      );
+      )
       const data = await response.json();
       console.log('request_token', data.request_token)
       return data.request_token;
     }
 
-    async function redirectToAuth() {
-      const token = await getRequestToken();
-      localStorage.setItem('temp_request_token', token);
+    async function handlerLogin(event, value) {
+      console.log(event, value)
+      const requestToken = await getRequestToken();
+
+      // Проверка учетных данных
+      const isValid = await validateCredentials(requestToken, username, password);
       
-      window.location.href = `https://www.themoviedb.org/authenticate/${token}?redirect_to=${encodeURIComponent(REDIRECT_URL)}`;
+      if (isValid) {
+        const sessionId = await createSession(requestToken);
+        localStorage.setItem('tmdb_session', sessionId);
+        alert('success');
+        loadUserData();
+      } else {
+        alert('error');
+      }
+    };
+
+    async function validateCredentials(token, username, password) {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/authentication/token/validate_with_login?api_key=${API_KEY}`,
+        {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            username: username,
+            password: password,
+            request_token: token
+          })
+        }
+      );
+      
+      return response.ok;
     }
-
-    // async function handleAuthCallback() {
-    //   const urlParams = new URLSearchParams(window.location.search);
-    //   const approved = urlParams.get('approved');
-    //   const requestToken = localStorage.getItem('temp_request_token');
-
-    //   if (approved === 'true' && requestToken) {
-    //     const sessionId = await createSession(requestToken);
-    //     localStorage.setItem('tmdb_session_id', sessionId);
-        
-    //     window.location.href = '/profile';
-    //   }
-    // }
-
-    // async function createSession(requestToken) {
-    //   const response = await fetch(
-    //     `https://api.themoviedb.org/3/authentication/session/new?api_key=${API_KEY}`,
-    //     {
-    //       method: 'POST',
-    //       headers: {'Content-Type': 'application/json'},
-    //       body: JSON.stringify({request_token: requestToken})
-    //     }
-    //   );
-    //   const data = await response.json();
-    //   console.log('session_id', data.session_id)
-    //   return data.session_id;
-    // }
 
 
 
